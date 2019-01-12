2508837911e06c47592e48d3e97418090f9a9e4b
Christopher Sams <csams@redhat.com>
2019-01-12 09:54:27 -0600

    Add nginx configuration parser.

---
+++ parseit/nginx_conf.py
@@ -0,0 +1,37 @@
+import string
+from parseit import (EOF, Forward, LeftCurly, Lift, RightCurly, Many, Number,
+                     OneLineComment, SemiColon, SingleQuotedString, String, WS)
+
+
+class Value:
+    def __init__(self, name, attrs, body):
+        self.name = name
+        self.attrs = attrs
+        self.body = body if body != ";" else []
+
+
+def skip_none(x):
+    return [i for i in x if i is not None]
+
+
+Comment = OneLineComment("#").map(lambda x: None)
+Stmt = Forward()
+BeginBlock = WS >> LeftCurly << WS
+EndBlock = WS >> RightCurly << WS
+Bare = String(set(string.printable) - (set(string.whitespace) | set("#;{}'\"")))
+Name = WS >> String(string.ascii_letters + "_") << WS
+Attr = WS >> (Number | Bare | SingleQuotedString) << WS
+Attrs = Many(Attr)
+Block = BeginBlock >> Many(Stmt).map(skip_none) << EndBlock
+Stanza = (Lift(Value) * Name * Attrs * (Block | SemiColon)) | Comment
+Stmt <= WS >> Stanza << WS
+Doc = Many(Stmt).map(skip_none)
+Top = Doc + EOF
+
+
+def loads(data):
+    return Top(data)[0]
+
+
+def load(f):
+    return loads(f.read())
